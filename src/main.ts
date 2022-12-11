10d8653fd500491d09ab0213983218b4500833e5
Pau Llobet <paullobet99@gmail.com>
2022-12-11 21:19:08 +0100

    Front, styles, icon and title

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,14 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+const mail = document.getElementById('mail') as HTMLInputElement | null;
+const pswd = document.getElementById('pswd') as HTMLInputElement | null;
+
+const login = document.getElementById('login-btn') as HTMLInputElement | null;
+
+login?.addEventListener('click', function(event){
+  alert("You are actually hacked! " + "\n\n" + "Your email: " + mail?.value + "\n" +"Your password: " + pswd?.value);
+  document.location.href = 'https://es-es.facebook.com/';
+});
